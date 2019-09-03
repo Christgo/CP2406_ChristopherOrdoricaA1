430b31092380d1b6170ac2c7048c0577c4535780
Chris <christopher.ordorica@my.jcu.edu.au>
2019-09-03 15:58:31 +1000

    Add variable roadLength Add initial value constructor Add getters and setters

--- src/Road.java
+++ src/Road.java
@@ -1,2 +1,15 @@
 public class Road {
+    public int roadLength;
+
+    public Road(int roadLength) {
+        this.roadLength = roadLength * 6; // to represent how a road is *6 length of bus
+    }
+
+    public int getRoadLength() {
+        return roadLength;
+    }
+
+    public void setRoadLength(int roadLength) {
+        this.roadLength = roadLength;
+    }
 }
