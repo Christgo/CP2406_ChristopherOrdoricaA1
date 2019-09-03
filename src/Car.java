4647f8a718741bd5101f54d04070cb8bfd10c260
Chris <christopher.ordorica@my.jcu.edu.au>
2019-09-03 15:57:30 +1000

    Add variables carLength and carPosition Add initial value constructor Add getters and setters

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,23 @@
 public class Car {
+    public int carLength;
+    public int carPosition;
+
+    public Car(int carLength) {
+        this.carLength = carLength;
+        int carPosition = 0;
+    }
+
+    public void setCarPosition(int carPosition) {
+        this.carPosition = carPosition;
+    }
+    public void setCarLength(int carLength) {
+        this.carLength = carLength;
+    }
+
+    public int getCarLength() {
+        return carLength;
+    }
+    public int getCarPosition() {
+        return carPosition;
+    }
 }
