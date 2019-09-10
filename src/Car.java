e440aff4ac3a272825f11f66f08869393706aae1
Chris <christopher.ordorica@my.jcu.edu.au>
2019-09-10 21:53:53 +1000

    Add carRoad

--- src/Car.java
+++ src/Car.java
@@ -1,10 +1,12 @@
 public class Car {
     public int carLength;
     public int carPosition;
+    public int carRoad;
 
     public Car(int carLength) {
         this.carLength = carLength;
         int carPosition = 0;
+        int carRoad = 0;
     }
 
     public void setCarPosition(int carPosition) {
