cdc1eea59a8af7abfdbcb47749505b0624c16d5b
Chris <christopher.ordorica@my.jcu.edu.au>
2019-09-10 21:55:11 +1000

    Add trafficLight object initialisation Add basic for loop for car movement

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,23 @@ public class Main {
 //        System.out.println("How long is a car");
 //        int carLength = scanner.nextInt();
         int carLength = 1;
+        TrafficLight trafficLight = new TrafficLight();
         Car car = new Car(carLength);
         Road road1 = new Road(5);
         Road road2 = new Road(5);
         Road[] roadArray = new Road[2];
         roadArray[0] = road1;
         roadArray[1] = road2;
+
+        for (int i = -1; i < 5; i++) {
+            car.carPosition = i;
+            System.out.println(car.carPosition);
+            if (car.carPosition == 4 && trafficLight.isGreen)
+                //make car go to other road
+                i = -1;
+        }
+
+
     }
 }
 
