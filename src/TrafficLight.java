2d5901c95c9eb909d16f8bac42b60ad474dbf97a
Chris <christopher.ordorica@my.jcu.edu.au>
2019-09-10 22:13:24 +1000

    Add method for randomly changing isGreen

--- src/TrafficLight.java
+++ src/TrafficLight.java
@@ -1,5 +1,13 @@
+import java.util.Random;
+
 public class TrafficLight {
-    boolean isGreen = true;
+    public boolean isGreen = true;
+    public Random random = new Random();
+
+    void changeLight() {
+        int num = random.nextInt(10);
+        this.isGreen = num > 5;
+    }
 }
 
 //randomly changed true/false value of particulat road segment?
