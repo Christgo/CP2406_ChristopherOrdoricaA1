725d31315728330d8e7724b1f46db49e425a925b
Chris <christopher.ordorica@my.jcu.edu.au>
2019-09-11 11:10:57 +1000

    Add test for TrafficLight

---
+++ src/TrafficLightTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrafficLightTest {
+    @Test void basicTest()  {
+        TrafficLight trafficLight = new TrafficLight();
+        trafficLight.setGreen(false);
+        assertFalse(trafficLight.getIsGreen());
+    }
+
+}
