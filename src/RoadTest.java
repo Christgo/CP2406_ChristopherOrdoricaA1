b90bee4fbc9f146e1bbb3f502d84016ad37540a8
Chris <christopher.ordorica@my.jcu.edu.au>
2019-09-12 12:21:35 +1000

    Complete roadTest

--- src/RoadTest.java
+++ src/RoadTest.java
@@ -1,5 +1,17 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RoadTest {
+    @Test void basicTest() {
+        Road road = new Road(5, 0);
+        assertEquals(5, road.roadLength);
+        assertEquals(0, road.roadNumber);
+
+        road.setRoadLength(4);
+        road.setRoadNumber(1);
+        assertEquals(4, road.getRoadLength());
+        assertEquals(1, road.getRoadNumber());
+    }
 
 }
