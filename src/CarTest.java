b696a3f2c3e69b26ba5dfd8fce23f677af25e810
Chris <christopher.ordorica@my.jcu.edu.au>
2019-09-12 12:26:09 +1000

    Complete CarTest

--- src/CarTest.java
+++ src/CarTest.java
@@ -1,5 +1,21 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
+    @Test
+    void basicTest() {
+        Car car = new Car(1);
+        assertEquals(1, car.carLength);
+        assertEquals(0, car.carPosition);
+        assertEquals(0, car.carRoad);
+
+        car.setCarLength(2);
+        car.setCarPosition(3);
+        car.setCarRoad(2);
+        assertEquals(2, car.getCarLength());
+        assertEquals(3, car.getCarPosition());
+        assertEquals(2, car.getCarRoad());
+    }
 
 }
