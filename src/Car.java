b38be6fe33b8586ba8bcf865960f885055cf652d
Chris <christopher.ordorica@my.jcu.edu.au>
2019-09-12 12:26:43 +1000

    Add getters and setters Format code

--- src/Car.java
+++ src/Car.java
@@ -9,17 +9,27 @@ public class Car {
         int carRoad = 0;
     }
 
-    public void setCarPosition(int carPosition) {
-        this.carPosition = carPosition;
+    public int getCarLength() {
+        return carLength;
     }
+
     public void setCarLength(int carLength) {
         this.carLength = carLength;
     }
 
-    public int getCarLength() {
-        return carLength;
-    }
     public int getCarPosition() {
         return carPosition;
+    }
+
+    public void setCarPosition(int carPosition) {
+        this.carPosition = carPosition;
+    }
+
+    public int getCarRoad() {
+        return carRoad;
+    }
+
+    public void setCarRoad(int carRoad) {
+        this.carRoad = carRoad;
     }
 }
