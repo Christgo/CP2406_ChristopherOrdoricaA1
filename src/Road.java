1511dac5b7dcfb7875eb46afb802d62e37898ecc
Chris <christopher.ordorica@my.jcu.edu.au>
2019-10-29 21:13:34 +1000

    fix: Fix code so that setRoadLength correctly functions
    add more code in order to allow for correct manipulation of roadLength

--- src/Road.java
+++ src/Road.java
@@ -4,7 +4,7 @@ public class Road {
 
 
     public Road(int roadLength, int roadNumber) {
-        this.roadLength = roadLength * 3; // cannot be larger that vehicleLength * 5 or less than vehicleLength * 2
+        this.roadLength = roadLength * 3 * 2; // cannot be larger that vehicleLength * 5 or less than vehicleLength * 2
         this.roadNumber = roadNumber;
     }
 
@@ -13,11 +13,10 @@ public class Road {
     }
 
     public void setRoadLength(int roadLength) {
-        if (roadLength > this.roadLength * 5) { //may not function as intended
-            this.roadLength *= 5;
+        if (roadLength > this.roadLength/2*5) { //TODO: streamline so that it can use a different variable
+            this.roadLength = this.roadLength/2*5;
             System.out.println("road length cannot exceed bus length * 5");
-        } else if (roadLength < this.roadLength * 2) {
-            this.roadLength *= 2;
+        } else if (roadLength < this.roadLength) {
             System.out.println("road length must be minimum of bus length * 2");
         } else
             this.roadLength = roadLength;
