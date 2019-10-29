5ed869717d9a10daea2957931100f97de11ad92e
Chris <christopher.ordorica@my.jcu.edu.au>
2019-10-29 20:00:48 +1000

    refactor: Change variables to match new names in Car class

--- src/CarTest.java
+++ src/CarTest.java
@@ -6,16 +6,16 @@ class CarTest {
     @Test
     void basicTest() {
         Car car = new Car(1);
-        assertEquals(1, car.carLength);
-        assertEquals(0, car.carPosition);
-        assertEquals(0, car.carRoad);
+        assertEquals(1, car.length);
+        assertEquals(0, car.position);
+        assertEquals(0, car.road);
 
-        car.setCarLength(2);
-        car.setCarPosition(3);
-        car.setCarRoad(2);
-        assertEquals(2, car.getCarLength());
-        assertEquals(3, car.getCarPosition());
-        assertEquals(2, car.getCarRoad());
+        car.setLength(2);
+        car.setPosition(3);
+        car.setRoad(2);
+        assertEquals(2, car.getLength());
+        assertEquals(3, car.getPosition());
+        assertEquals(2, car.getRoad());
     }
 
 }
