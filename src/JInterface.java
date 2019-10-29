9673fdc1dcd14ec6a4dbeb2fa224ccfa1b819e06
Chris <christopher.ordorica@my.jcu.edu.au>
2019-10-29 14:04:44 +1000

    style: Reorder code segments
    segments of code now relate to each other by panel, not by function

--- src/JInterface.java
+++ src/JInterface.java
@@ -11,13 +11,20 @@ public class JInterface extends JPanel implements ActionListener, ItemListener {
 
     public JInterface() {
         super();
+        JLabel topLabel = new JLabel("label1");
         JPanel topPanel = new JPanel();
-        JPanel middlePanel = new JPanel();
-        JPanel bottomPanel = new JPanel();
-
         topPanel.setBackground(Color.BLACK);
+        topPanel.add(topLabel);
+
+        JLabel middleLabel = new JLabel("label2");
+        JPanel middlePanel = new JPanel();
         middlePanel.setBackground(Color.GREEN);
+        middlePanel.add(middleLabel);
+
+        JLabel bottomLabel = new JLabel("label3");
+        JPanel bottomPanel = new JPanel();
         bottomPanel.setBackground(Color.RED);
+        bottomPanel.add(bottomLabel);
 
 //        topPanel.setPreferredSize(new Dimension(300,300));
 //        middlePanel.setPreferredSize(new Dimension(300,300));
