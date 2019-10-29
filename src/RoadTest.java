37c8cd34a1b1cb107a6366f423d7c861a9155c69
Chris <christopher.ordorica@my.jcu.edu.au>
2019-10-29 21:15:40 +1000

    fix: Add test code in order to assert setRoadLength correctly

--- src/RoadTest.java
+++ src/RoadTest.java
@@ -5,13 +5,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class RoadTest {
     @Test
     void basicTest() {
-        Road road = new Road(5, 0);
-        assertEquals(5, road.roadLength);
+        Road road = new Road(2, 0); // default vehicleLength is 2
+        assertEquals(12, road.roadLength);
         assertEquals(0, road.roadNumber);
 
-        road.setRoadLength(4);
+        road.setRoadLength(11);
+        assertEquals(12, road.getRoadLength());
+        road.setRoadLength(12);
+        assertEquals(12, road.getRoadLength());
+        road.setRoadLength(13);
+        assertEquals(13, road.getRoadLength());
+        road.setRoadLength(31);
+        assertEquals(30, road.getRoadLength());
         road.setRoadNumber(1);
-        assertEquals(4, road.getRoadLength());
         assertEquals(1, road.getRoadNumber());
     }
 
