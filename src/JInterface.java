b9eedc3296e38c3042dfe3b56ba30ef356d85d47
Chris <christopher.ordorica@my.jcu.edu.au>
2019-11-01 21:57:25 +1000

    style: Change labels and add colours
    Change label names to meaningful names and change colours to meaningful
    colours

--- src/JInterface.java
+++ src/JInterface.java
@@ -12,26 +12,28 @@ public class JInterface extends JPanel implements ActionListener, ItemListener {
     JInterface() {
         super();
 
-        JLabel topLabel = new JLabel("label1");
-        String[] vehicleLengths = {"2","4","6","8"};
+        JLabel topLabel = new JLabel("Vehicle Length");
+        String[] vehicleLengths = {"2","4","6","8"}; //preset values for vehicle config
         JComboBox<String> vehicleLength = new JComboBox<>(vehicleLengths);
         JPanel topPanel = new JPanel();
+        topLabel.setForeground(Color.WHITE);
         topPanel.setBackground(Color.BLACK);
         topPanel.add(topLabel);
 
-        vehicleLength.setSelectedIndex(0);
+        vehicleLength.setSelectedIndex(0); //always default on "2"
         vehicleLength.addActionListener(this);
         topPanel.add(vehicleLength);
 
         JLabel middleLabel = new JLabel("label2");
         JPanel middlePanel = new JPanel();
         middlePanel.setLayout(new GridLayout(3,3));
-        middlePanel.setBackground(Color.GREEN);
+        middlePanel.setBackground(Color.WHITE);
         middlePanel.add(middleLabel);
 
-        JLabel bottomLabel = new JLabel("label3");
+        JLabel bottomLabel = new JLabel("Stats:");
         JPanel bottomPanel = new JPanel();
-        bottomPanel.setBackground(Color.RED);
+        bottomLabel.setForeground(Color.WHITE);
+        bottomPanel.setBackground(Color.BLACK);
         bottomPanel.add(bottomLabel);
 
 //        topPanel.setPreferredSize(new Dimension(300,300));
